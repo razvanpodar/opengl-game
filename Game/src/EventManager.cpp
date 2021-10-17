2a91f20c3e8097cb2e97ec5a0222c75590ec622d
razvanpodar <razvan_podar@yahoo.com>
2021-10-17 14:43:57 +0300

    Add event manager

---
+++ Game/src/EventManager.cpp
@@ -0,0 +1,7 @@
+#include "EventManager.h"
+
+void EventManager::ProcessInputs(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
---
+++ Game/src/EventManager.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <GLFW/glfw3.h>
+
+class EventManager
+{
+public:
+	void ProcessInputs(GLFWwindow *window);
+};
