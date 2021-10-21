c96f11b273dbcd9f9b3be5cb0b56032432a588aa
razvanpodar <razvan_podar@yahoo.com>
2021-10-21 22:24:14 +0300

    Implement directional light

--- Game/shaders/lightShader.fs
+++ Game/shaders/lightShader.fs
@@ -8,8 +8,9 @@ struct Material
 }; 
 
 struct Light {
-    vec3 position;
-  
+    // vec3 position;
+	vec3 direction;
+
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
@@ -36,7 +37,8 @@ void main()
 
 	// Diffuse light
 	vec3 norm = normalize(normal);
-	vec3 lightDir = normalize(lightPos - fragPos);
+	// vec3 lightDir = normalize(lightPos - fragPos);
+	vec3 lightDir = normalize(-light.direction);
 	float diff = max(dot(norm, lightDir), 0.0);
 	// vec3 diffuse = lightColor * (diff * material.diffuse);
 	vec3 diffuse  = light.diffuse * diff * vec3(texture(material.diffuse, texCoords));
--- Game/src/Game.cpp
+++ Game/src/Game.cpp
@@ -113,6 +113,19 @@ int main()
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
     };
 
+    glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f,  0.0f,  0.0f),
+    glm::vec3(2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f,  2.0f, -2.5f),
+    glm::vec3(1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
     unsigned int VBO, lightVAO;
     glGenVertexArrays(1, &lightVAO);
     glGenBuffers(1, &VBO);
@@ -171,11 +184,12 @@ int main()
         lightShader.Use();
 
         lightShader.SetUniform3fv("viewPos", camera.m_position);
-        lightShader.SetUniform3fv("light.position", lightPos);
+        //lightShader.SetUniform3fv("light.position", lightPos);
 
         lightShader.SetUniform3fv("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
         lightShader.SetUniform3fv("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
-        lightShader.SetUniform3fv("lightPos", lightPos);
+        //lightShader.SetUniform3fv("lightPos", lightPos);
+        lightShader.SetUniform3fv("light.direction", glm::vec3(-0.2f, -1.0f, -0.3f));
         lightShader.SetUniform3fv("material.specular", glm::vec3(0.5f, 0.5f, 0.5f));
         lightShader.SetUniform1f("material.shininess", 64.0f);
 
@@ -196,9 +210,21 @@ int main()
         textureManager.BindTexture(0);
         textureManager.BindTexture(1);
 
-        // Draw
         glBindVertexArray(lightVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            lightShader.SetUniformMatrix4fv("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        // Draw
+        //glDrawArrays(GL_TRIANGLES, 0, 36);
         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         lightSourceShader.Use();
